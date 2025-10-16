8a69ad954b9aac468b526ee7c363015e3ec9990a
Elias Milano <eliasmilano.dev@gmail.com>
2025-10-16 12:41:48 -0300

    added 'clientes' and 'vendedores' templates and views

--- Fabrica/urls.py
+++ Fabrica/urls.py
@@ -22,5 +22,8 @@ from planificacion import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path ("", views.index, name="index"),
-    path ("proyectos/", views.proyectos, name="proyectos")
+    path ("proyectos/", views.proyectos, name="proyectos"),
+    path ("clientes/", views.clientes, name="clientes"),
+    path ("vendedores/", views.vendedores, name="vendedores"),
+
 ]
--- planificacion/views.py
+++ planificacion/views.py
@@ -3,9 +3,16 @@ from .models import Cliente, Vendedor, Proyecto
 
 
 def index(request):
-    lista_proyectos = Proyecto.objects.all()
-    return render(request, "planificacion/index.html", context={"proyectos": lista_proyectos})
+    return render(request, "planificacion/index.html")
 
 def proyectos(request):
     lista_proyectos = Proyecto.objects.all()
     return render(request, "planificacion/proyectos.html", context={"proyectos": lista_proyectos})
+
+def clientes(request):
+    lista_clientes = Cliente.objects.all()
+    return render(request, "planificacion/clientes.html", context={"clientes": lista_clientes})
+
+def vendedores(request):
+    lista_vendedores = Vendedor.objects.all()
+    return render(request, "planificacion/vendedores.html", context={"vendedores": lista_vendedores})

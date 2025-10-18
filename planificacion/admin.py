1e204b75583bcf03a2827441cdb7cb0434bb55e3
Elias Milano <eliasmilano.dev@gmail.com>
2025-10-18 14:29:48 -0300

    added administrator session configuration in admin.py

--- planificacion/admin.py
+++ planificacion/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+#admin.site.register(Proyecto)   Visualización básica
+
+@admin.register(Proyecto)
+class ProyectoAdmin(admin.ModelAdmin):
+    list_display = ["numero_proyecto", "fecha_inicio", "fecha_entrega", "fecha_ingreso", "cliente", "vendedor"]
+    list_filter = ["numero_proyecto", "fecha_inicio", "fecha_entrega", "fecha_ingreso", "cliente", "vendedor"]
+    raw_id_fields = ["cliente", "vendedor"]
+    ordering = ["-numero_proyecto"]
+
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ["nombre", "apellido", "direccion", "telefono", "email"]
+    list_filter = ["nombre", "apellido"]
+    ordering = ["nombre"]
+
+
+@admin.register(Vendedor)
+class VendedorAdmin(admin.ModelAdmin):
+    list_display = ["nombre", "apellido", "telefono", "email"]
+    list_filter = ["nombre", "apellido"]
+    ordering = ["nombre"]

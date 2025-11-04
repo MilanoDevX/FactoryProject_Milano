8fc2306d06be58657507f585d1f09f12ce9b8add
Elias Milano <eliasmilano.dev@gmail.com>
2025-11-04 12:37:39 -0300

    changes in Django admin panel

--- cuentas/admin.py
+++ cuentas/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+@admin.register(Perfil)
+class PerfilAdmin(admin.ModelAdmin):
+    list_display = ["username", "first_name", "last_name", "email", "is_staff", "is_superuser", "pais", "fecha_de_nacimiento", "direccion"]
+    list_filter = ["username", "email", "is_staff", "is_superuser", "pais",]
+    ordering = ["username"]
+
--- produccion/admin.py
+++ produccion/admin.py
@@ -5,7 +5,7 @@ from .models import *
 
 @admin.register(Produccion)
 class ProduccionAdmin(admin.ModelAdmin):
-    list_display = ["numero_proyecto", "horas_disenio_teo", "horas_fabricacion_teo", "horas_montaje_teo", "horas_disenio_real", "horas_fabricacion_real", "horas_montaje_real"]
-    list_filter = ["numero_proyecto", "horas_disenio_teo", "horas_fabricacion_teo", "horas_montaje_teo", "horas_disenio_real", "horas_fabricacion_real", "horas_montaje_real"]
+    list_display = ["numero_proyecto","horas_disenio_real", "horas_fabricacion_real", "horas_montaje_real", "incidencias"]
+    list_filter = ["numero_proyecto", "horas_disenio_real", "horas_fabricacion_real", "horas_montaje_real"]
     ordering = ["-numero_proyecto"]
 

47279c57ae58a95e397767e4706ca50f5d89d5ff
Elias Milano <eliasmilano.dev@gmail.com>
2025-11-04 13:07:25 -0300

    added views for login and logout in 'cuentas' app

--- cuentas/urls.py
+++ cuentas/urls.py
@@ -5,8 +5,10 @@ from cuentas.views import *
 app_name = "cuentas"
 
 urlpatterns = [
-    path("login/", LoginView.as_view(template_name="cuentas/login.html", redirect_authenticated_user=True, next_page="cuentas:profile_detail"), name="login"),
-    path("logout/", LogoutView.as_view(template_name="cuentas/logout.html"), name="logout"),
+    # path("login/", LoginView.as_view(template_name="cuentas/login.html", redirect_authenticated_user=True, next_page="cuentas:profile_detail"), name="login"),
+    # path("logout/", LogoutView.as_view(template_name="cuentas/logout.html"), name="logout"),
+    path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
     path("register/", register, name="register"),
     path("profile/", profile_detail, name="profile_detail"),
     path("profile/edit/", profile_edit, name="profile_edit"),
--- cuentas/views.py
+++ cuentas/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from cuentas.forms import PerfilCreationForm, PerfilChangeForm
 
@@ -32,3 +34,36 @@ def profile_edit(request):
         form = PerfilChangeForm(instance=request.user)
     return render(request, "cuentas/perfil_editar.html", {"form": form})
 
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect("cuentas:profile_detail")
+
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f"¡Bienvenido {user.username}!")
+                return redirect("/")
+            else:
+                messages.error(request, "Usuario o contraseña incorrectos.")
+        else:
+            messages.error(request, "Datos inválidos. Intenta nuevamente.")
+    else:
+        form = AuthenticationForm()
+    return render(request, "cuentas/login.html", {"form": form})
+
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return render(request, "cuentas/logout.html")
